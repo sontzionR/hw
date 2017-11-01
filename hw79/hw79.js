39cf8bf44a7d3822743bbd401aa6223911fa4650
sontzionR <sontzion@gmail.com>
2017-10-31 21:34:46 -0400

    hw79

---
+++ hw79/hw79.js
@@ -0,0 +1,15 @@
+(function(){
+    var theBody = document.body;
+    // var fontChooser = document.getElementById("fontColor").value;
+    // var backgroundChooser = document.getElementById("backgroundColor").value;
+
+    var theButton = document.getElementById("theButton");
+    theButton.addEventListener('click', function () {
+        var fontChooser = document.getElementById("fontColor").value;
+        var backgroundChooser = document.getElementById("backgroundColor").value;
+        theBody.style.color = fontChooser;
+        theBody.style.backgroundColor = backgroundChooser;
+    });
+
+}());    
+
