38ad131739d846b61085b9c445bae028849a3909
sontzionR <sontzion@gmail.com>
2017-11-22 14:25:40 -0500

    hw85

--- hw85/hw85.js
+++ hw85/hw85.js
@@ -10,10 +10,10 @@
         setTimeout(function () {
             $.get(fileName, function (file) {
                 loader.hide();
-                $("#theFile").append(file);
+                $("#theFile").text(file).css("background-color","green");;
             }).fail(function (xhr, statusCode, statusText) {
                 loader.hide();
-                 $("#theFile").append(statusCode,statusText).css("background-color","red");
+                 $("#theFile").text(statusCode,statusText).css("background-color","red");
                 //  alert("error: " + statusText);
                 // console.log(xhr, statusCode, statusText);
                 //cant get it to work  $.get(/*"/HTML/HW/class/JavaScript/83/messageBox.js"*/"/HTML/HW/class/JavaScript/81/messageBox.js",function(d){
