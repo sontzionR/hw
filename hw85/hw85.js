99693fb78efcdce6740ed10767a32e39ed2f63d8
sontzionR <sontzion@gmail.com>
2017-11-22 14:01:27 -0500

    hw 85(no message box)

--- hw85/hw85.js
+++ hw85/hw85.js
@@ -1,28 +1,27 @@
 /*global $ */
 (function () {
     "use strict";
-    var loader = $(".loader");
-    //  loader.hide();
-    var fileName = $("#fileName");
-    fileName = fileName.val();
-    var button = $("#theButton");
-    button.click(function (){
-            loader.show();
+   
+    $("#theButton").click(function (){
+        var loader = $(".loader");
+        loader.css({"border": "6px solid #f3f3f3","border-radius": "50%","border-top": "6px solid #3498db","width": "15px","height": "15px", "-webkit-animation": "spin 1s linear infinite", "animation": "spin 1s linear infinite"}).show();
         var fileName = $("#fileName");
         fileName = fileName.val();
-        $.get(fileName, function (fileName) {
-            // alert(fileName);
-            loader.hide();
-            $("#theFile").text(fileName);
-        }).fail(function (xhr, statusCode, statusText) {
-            loader.hide();
-            $("#theFile").append(statusCode,statusText);
-            // alert("error: " + statusText);
-            // console.log(xhr, statusCode, statusText);
-            //hope to finish this e.c. tomorrow...  $.get("/HTML/HW/class/JavaScript/83/messageBox.js",function(){
-            // })
+        setTimeout(function () {
+            $.get(fileName, function (file) {
+                loader.hide();
+                $("#theFile").append(file);
+            }).fail(function (xhr, statusCode, statusText) {
+                loader.hide();
+                 $("#theFile").append(statusCode,statusText).css("background-color","red");
+                //  alert("error: " + statusText);
+                // console.log(xhr, statusCode, statusText);
+                //cant get it to work  $.get(/*"/HTML/HW/class/JavaScript/83/messageBox.js"*/"/HTML/HW/class/JavaScript/81/messageBox.js",function(d){
+               //  pcs.messageBox.show("error");
+                //  });
 
-        });
+            });
+        },2000);    
     });
 
 }());
